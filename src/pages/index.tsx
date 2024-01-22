f72a255b1e272ec3f960a032fedcfd28e350f4c0
Milena Mijovic <2103milena@gmail.com>
2024-01-22 23:03:34 +0100

    bg image opacity

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,19 @@ export default function Home() {
     hello: t("common:hello"),
   };
   return (
-    <div>
+    <div className="relative h-screen">
 
-      <div className="bg-custom-background bg-cover bg-center h-screen">
-        <div className={"font-whisper"}>{localizedStrings.world}</div>
-        <div>{localizedStrings.hello}</div>
+      <div className="absolute inset-0 bg-custom-background bg-cover bg-center opacity-40"></div>
+
+      <div className="relative z-40 flex items-center justify-center h-full">
+        <div>
+          <div className="font-whisper">{localizedStrings.world}</div>
+          <div>{localizedStrings.hello}</div>
+        </div>
       </div>
+
     </div>
+
   );
 }
 
